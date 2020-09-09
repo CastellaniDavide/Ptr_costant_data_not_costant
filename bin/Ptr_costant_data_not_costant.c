11a436a61b5af89e16c036e3218b4655f247f425
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-09 08:58:12 +0200

    Created bin/template.c

---
+++ bin/Ptr_costant_data_not_costant.c
@@ -0,0 +1,22 @@
+/**
+ * @file Ptr_costant_data_not_costant.cpp
+ *
+ * @version 01.01 2020-08-15
+ *
+ * @brief Puntator costant and data not costant in c
+ *
+ * @ingroup Ptr_costant_data_not_costant
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+#include <stdio.h>
+
+int main() {
+	// printf() displays the string inside quotation
+	printf("Ptr_costant_data_not_costant");
+	return 0;
+}
